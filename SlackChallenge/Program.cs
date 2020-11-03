e1b912246c7e6a7b02d85c61131c58112b17624e
Alex Soto <alex.soto0624@gmail.com>
2020-11-03 13:51:52 -0800

    added slack challenge

---
+++ SlackChallenge/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace SlackChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Multiword Term:");
+            string input = Console.ReadLine();
+            string[] words = input.Split(' ');
+            String acronym = "";
+            foreach (var item in words)
+            {
+               acronym += item.ToUpper()[0];
+
+            }
+            Console.WriteLine($"{acronym}");
+
+
+
+        }
+    }
+}
